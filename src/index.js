49d9d1ca0d9cc161d4df8b37f2e1cb28c58306dd
Aket123 <aketzalimatus@gmail.com>
2023-10-06 10:23:02 -0600

    Proyecto finalizado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+// Borrar información
+const button = document.getElementById("reset-button");
+const textarea = document.querySelector('textarea[name="user-input"]');
+button.addEventListener("click", function () {
+  textarea.value = "";
+});
+
+// METRICAS
+
+// 1.Número de caracteres
+textarea.addEventListener("input", function () {
+  const numeroCaracteres = analyzer.getCharacterCount(textarea.value);
+  document.querySelector("li.metrica:nth-child(2)").textContent =
+    "Caracteres: " + numeroCaracteres;
+  // 2.Número de palabras
+  const wordCount = analyzer.getWordCount(textarea.value);
+  document.querySelector("li.metrica:nth-child(1)").textContent =
+    "Palabra: " + wordCount;
+  // 3.Caracteres excluyendo espacios y signos de puntuación
+  const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(
+    textarea.value
+  );
+  document.querySelector("li.metrica:nth-child(3)").textContent =
+    "Caracteres sin espacios ni de puntuación: " +
+    characterCountExcludingSpaces;
+  // 4.Recuento de números
+  const numberCount = analyzer.getNumberCount(textarea.value);
+  document.querySelector("li.metrica:nth-child(4)").textContent =
+    "Números: " + numberCount;
+  // 5.Suma total de números
+  const numbersum = analyzer.getNumberSum(textarea.value);
+  document.querySelector("li.metrica:nth-child(5)").textContent =
+    "Suma de números: " + numbersum;
+  // 6.Longitud media de palabras
+  const wordlengthaverage = analyzer.getAverageWordLength(textarea.value);
+  document.querySelector("li.metrica:nth-child(6)").textContent =
+    "Longitud media de palabras: " + wordlengthaverage;
+});
